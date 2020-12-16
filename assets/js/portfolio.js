cec5c229951377925d72418d8ac20f724bb25374
yanick <you@example.com>
2020-12-16 12:23:42 +0100

    amelioration sur le responsive sur les versions mobiles

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,9 @@
 // Initialisation de notre portfolio, toutes les variables
 
+// const parallax = document.querySelector('.main-realisation');
+// const box = document.querySelector('.box-realisation');
 
-
-
+// window.addEventListener('scroll', () => {
+//     // parallax.style.backgroundPositionY = window.scrollY / 2 + "px"
+//     console.log(window.scrollY / 2)
+// })
